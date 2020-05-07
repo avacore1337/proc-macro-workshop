a4814c5328e1b2ba673acecc3c84ec5a801c4d98
Robert Welin-Berger <avacore1337@gmail.com>
2020-05-07 19:34:06 +0200

    01 done

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,13 @@
 extern crate proc_macro;
 
+use syn::{parse_macro_input, DeriveInput};
+
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let ast = parse_macro_input!(input as DeriveInput);
+    eprintln!("TOKENS: {:#?}", ast);
+    TokenStream::new()
 }
