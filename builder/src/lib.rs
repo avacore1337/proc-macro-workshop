3373f80f2842ce5052fc81295a77d6d1acc9affa
Robert Welin-Berger <avacore1337@gmail.com>
2020-05-07 20:08:02 +0200

    02 done

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,37 @@
 extern crate proc_macro;
 
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 use proc_macro::TokenStream;
+// use proc_macro2::{Ident, Span};
 use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     eprintln!("TOKENS: {:#?}", ast);
-    TokenStream::new()
+
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = Ident::new(&bname, name.span());
+    let expanded = quote! {
+        pub struct #bident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #name {
+            pub fn builder() -> #bident{
+                #bident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    expanded.into()
 }
