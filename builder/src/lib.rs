d1addab9bf1d298676c07d789dc07e12cf345c9d
Robert Welin-Berger <avacore1337@gmail.com>
2020-05-09 15:34:58 +0200

    implemented generic fields

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -10,7 +10,7 @@ use quote::quote;
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    // eprintln!("TOKENS: {:#?}", ast);
+    eprintln!("TOKENS: {:#?}", ast);
 
     let name = &ast.ident;
     let bname = format!("{}Builder", name);
@@ -31,6 +31,52 @@ pub fn derive(input: TokenStream) -> TokenStream {
         quote! { #name: std::option::Option<#ty> }
     });
 
+    let builder_init = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! { #name: None }
+    });
+
+    let builder_methods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
+    let builder_params = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! { #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))? }
+        // let ty = &f.ty;
+        // if option_type(ty).is_some() {
+        //     quote! { #name: self.#name.clone()}
+        // } else {
+        //     quote! { #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))? }
+        // }
+        // ty: Path(
+        //                         TypePath {
+        //                             qself: None,
+        //                             path: Path {
+        //                                 leading_colon: None,
+        //                                 segments: [
+        //                                     PathSegment {
+        //                                         ident: Ident {
+        //                                             ident: "Option",
+        //                                             span: #0 bytes(2884..2890),
+        //                                         }},
+        //                                         arguments: AngleBracketed(
+        //                                             AngleBracketedGenericArguments {
+        //                                                 colon2_token: None,
+        //                                                 lt_token: Lt,
+        //                                                 args: [
+        //                                                     Type(
+        //                                                         Path(
+        //                                                             TypePath {
+    });
+
     let expanded = quote! {
         pub struct #bident {
             #(#builder_fields,)*
@@ -38,36 +84,19 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #name {
             pub fn builder() -> #bident{
                 #bident {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#builder_init,)*
                 }
             }
         }
         impl #bident {
-            pub fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
+            #(#builder_methods)*
             pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
                 Ok(#name{
-                    executable: self.executable.clone().ok_or("executable is not set")?,
-                    args: self.args.clone().ok_or("args is not set")?,
-                    env: self.env.clone().ok_or("env is not set")?,
-                    current_dir: self.current_dir.clone().ok_or("current_dir is not set")?,
+                    #(#builder_params,)*
+                    // executable: self.executable.clone().ok_or("executable is not set")?,
+                    // args: self.args.clone().ok_or("args is not set")?,
+                    // env: self.env.clone().ok_or("env is not set")?,
+                    // current_dir: self.current_dir.clone().ok_or("current_dir is not set")?,
                 })
             }
         }
@@ -75,3 +104,17 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     expanded.into()
 }
+
+fn option_type<'a>(ty: &'a syn::Type) -> Option<&'a syn::Type> {
+    // let Path{TypePath segment PathSegment { Ident { ident "Option", args }
+    //             = ty { args.get(0)}
+    //             else {
+    //                 ty
+    //             }
+
+    Some(ty)
+}
+
+// fn ty_inner_type<'a>(wrapper: &str, ty: &'a syn::Type) -> Option<&'a syn::Type> {
+//     None
+// }
