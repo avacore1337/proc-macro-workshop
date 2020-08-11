b2f02141ef5714fd3758c69c6b2ce8c20c69eec5
Robert Welin-Berger <avacore1337@gmail.com>
2020-08-11 18:57:27 +0200

    finished 07

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -7,7 +7,7 @@ use syn::{parse_macro_input, DeriveInput, Ident};
 use proc_macro::TokenStream;
 use quote::quote;
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     eprintln!("TOKENS: {:#?}", ast);
@@ -28,7 +28,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_fields = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if let Some(_) = option_type(ty) {
+        if builder_of(f).is_some() {
+            if let Some(v_ty) = vec_type(ty) {
+                quote! { #name: std::vec::Vec<#v_ty> }
+            } else {
+                unimplemented!();
+            }
+        } else if let Some(_) = option_type(ty) {
             quote! { #name: #ty }
         } else {
             quote! { #name: std::option::Option<#ty> }
@@ -37,13 +43,32 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let builder_init = fields.iter().map(|f| {
         let name = &f.ident;
-        quote! { #name: None }
+        if builder_of(f).is_some() {
+            quote! { #name: std::vec::Vec::new() }
+        } else {
+            quote! { #name: std::option::Option::None }
+        }
     });
 
     let builder_methods = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if let Some(i_ty) = option_type(ty) {
+        if let Some(attr) = builder_of(f) {
+            if let Some(method_name) = get_each_name(attr) {
+                if let Some(v_ty) = vec_type(ty) {
+                    quote! {
+                        pub fn #method_name(&mut self, #name: #v_ty) -> &mut Self {
+                            self.#name.push(#name);
+                            self
+                        }
+                    }
+                } else {
+                    unimplemented!();
+                }
+            } else {
+                unimplemented!();
+            }
+        } else if let Some(i_ty) = option_type(ty) {
             quote! {
                 pub fn #name(&mut self, #name: #i_ty) -> &mut Self {
                     self.#name = Some(#name);
@@ -63,7 +88,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_params = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if option_type(ty).is_some() {
+        if option_type(ty).is_some() || builder_of(f).is_some() {
             quote! { #name: self.#name.clone()}
         } else {
             quote! { #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))? }
@@ -94,11 +119,81 @@ pub fn derive(input: TokenStream) -> TokenStream {
     expanded.into()
 }
 
-fn option_type<'a>(ty: &'a syn::Type) -> Option<&'a syn::Type> {
+fn get_each_name(a: &syn::Attribute) -> Option<Ident> {
+    if let Ok(syn::Meta::List(meta)) = a.parse_meta() {
+        eprintln!("META: {:#?}", meta);
+        if let syn::NestedMeta::Meta(syn::Meta::NameValue(ref name_value_meta)) =
+            meta.nested.first()?
+        {
+            if name_value_meta.path.segments.first()?.ident != "each" {
+                return None;
+            }
+            if let syn::Lit::Str(ref s) = name_value_meta.lit {
+                // unimplemented!();
+                return Some(Ident::new(&s.value(), s.span()));
+            }
+        }
+    }
+
+    None
+}
+fn builder_of(f: &syn::Field) -> Option<&syn::Attribute> {
+    for attr in &f.attrs {
+        let segment = attr.path.segments.first()?;
+        if segment.ident == "builder" {
+            return Some(attr);
+        }
+    }
+    None
+}
+
+// META: MetaList {
+//     path: Path {
+//         leading_colon: None,
+//         segments: [
+//             PathSegment {
+//                 ident: Ident {
+//                     ident: "builder",
+//                     span: #0 bytes(1466..1473),
+//                 },
+//                 arguments: None,
+//             },
+//         ],
+//     },
+//     paren_token: Paren,
+//     nested: [
+//         Meta(
+//             NameValue(
+//                 MetaNameValue {
+//                     path: Path {
+//                         leading_colon: None,
+//                         segments: [
+//                             PathSegment {
+//                                 ident: Ident {
+//                                     ident: "each",
+//                                     span: #0 bytes(1474..1478),
+//                                 },
+//                                 arguments: None,
+//                             },
+//                         ],
+//                     },
+//                     eq_token: Eq,
+//                     lit: Str(
+//                         LitStr {
+//                             token: "env",
+//                         },
+//                     ),
+//                 },
+//             ),
+//         ),
+//     ],
+// }
+
+fn get_subtype<'a>(ty: &'a syn::Type, name: &str) -> Option<&'a syn::Type> {
     if let syn::Type::Path(ref p) = ty {
         if p.path.segments.len() == 1 {
             let p_seg = p.path.segments.first().unwrap();
-            if p_seg.ident == "Option" {
+            if p_seg.ident == name {
                 if let syn::PathArguments::AngleBracketed(ref angle) = p_seg.arguments {
                     if angle.args.len() != 1 {
                         return None;
@@ -111,4 +206,11 @@ fn option_type<'a>(ty: &'a syn::Type) -> Option<&'a syn::Type> {
         }
     }
     None
+}
+fn option_type<'a>(ty: &'a syn::Type) -> Option<&'a syn::Type> {
+    get_subtype(ty, "Option")
+}
+
+fn vec_type<'a>(ty: &'a syn::Type) -> Option<&'a syn::Type> {
+    get_subtype(ty, "Vec")
 }
